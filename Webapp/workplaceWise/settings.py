e130d34725de475690662d8b582cad24602af6c6
Carson Murray <cam50@email.sc.edu>
2024-02-23 15:39:37 -0500

    added requirements.txt and settings.py

--- Webapp/workplaceWise/settings.py
+++ Webapp/workplaceWise/settings.py
@@ -11,8 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from django.core.management.utils import get_random_secret_key
 import os
-from datetime import timedelta
+import dj_database_url
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -22,13 +24,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-hm(gzev_&1li*6^9i-t^0m^!3fmwn3)#gqnybp!@m#ou))i6ww'
+# SECRET_KEY = 'django-insecure-hm(gzev_&1li*6^9i-t^0m^!3fmwn3)#gqnybp!@m#ou))i6ww'
+SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", get_random_secret_key())
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = ['127.0.0.1','localhost']
-
+# DEBUG = os.getenv("DEBUG", "False") == "True"
+# ALLOWED_HOSTS = ['127.0.0.1','localhost']
+ALLOWED_HOSTS = os.getenv("127.0.0.1,localhost", "workplace-wise-27kxy.ondigitalocean.app", "workplacewise.site").split(",")
 # Application definition
 
 INSTALLED_APPS = [
@@ -38,22 +41,29 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Libraries
     'rest_framework',
+    'frontend.apps.FrontendConfig',
     'knox',
-    # Custom apps
-    'frontend.apps',
-    'accounts_app',
+    # following not in original settings.py 2/23/24
+    # 'accounts_app.apps.AccountsAppConfig',
     'tasks_app',
     'calendar_app',
     'discussions_app',
-    'performance_app',
-    
+    'performance_app',    
+    # Added as of 2/23/24
+    'frontend.apps',
+    'accounts_app',
     
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',
+                                       'rest_framework.authentication.SessionAuthentication',)
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,12 +96,13 @@ WSGI_APPLICATION = 'workplaceWise.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 
 
 # Password validation
@@ -128,7 +139,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
+STATIC_URL = "/static/"
+# STATIC_ROOT = os.path.join(BASE_DIR, "frontend/static/frontend/")\
+STATIC_ROOT = os.path.join(BASE_DIR, "frontend", "static", "frontend")
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")] 
+
 
 # Directory where uploaded profile pictures will be saved
 MEDIA_ROOT= os.path.join(BASE_DIR, 'media')
@@ -141,18 +157,45 @@ MEDIA_URL = '/media/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+DEVELOPMENT_MODE = os.getenv("DEVELOPMENT_MODE", "False") == "True"
+# DEVELOPMENT_MODE = True
+
+if DEVELOPMENT_MODE is True:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+        }
+    }
+elif len(sys.argv) > 0 and sys.argv[1] != 'collectstatic':
+    if os.getenv("DATABASE_URL", None) is None:
+        raise Exception("DATABASE_URL environment variable not defined")
+    DATABASES = {
+        "default": dj_database_url.parse(os.environ.get("DATABASE_URL")),
+    }
+
+SECURE_HSTS_SECONDS = 31536000
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD = True
 '''
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = '' #application email
-EMAIL_HOST_PASSWORD = '' #password for email
+# EMAIL_HOST_USER = '' #application email
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+# EMAIL_HOST_PASSWORD = '' #password for email
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+
 DEFAULT_FROM_EMAIL = '' #application email
 '''
-# EMAIL
-# Email account: workplacewise.testacc@gmail.com
-# App password: hlia kbel wmyd bkvc
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+# Added Email settings for password reset in beta release:
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
@@ -160,12 +203,5 @@ EMAIL_HOST_USER = 'workplacewise.testacc@gmail.com'
 EMAIL_HOST_PASSWORD = 'hliakbelwmydbkvc'
 EMAIL_USE_TLS = True
 
-REST_FRAMEWORK = {
-    'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
-}
-
-REST_KNOX = {
-    'USER_SERIALIZER': 'accounts.serializers.UserSerializer',
-    'TOKEN_TTL': timedelta(hours=48)
-}
+# Added custom user model in Beta Release:
 AUTH_USER_MODEL = 'accounts_app.CustomUser'
