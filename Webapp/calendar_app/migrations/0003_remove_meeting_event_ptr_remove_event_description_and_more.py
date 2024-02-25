1803f352ee3ed6e71c910cf05c2ffa4ff642c2f7
Carson Murray <cam50@email.sc.edu>
2024-02-25 01:11:54 -0500

    comment id in migration 3

--- Webapp/calendar_app/migrations/0003_remove_meeting_event_ptr_remove_event_description_and_more.py
+++ Webapp/calendar_app/migrations/0003_remove_meeting_event_ptr_remove_event_description_and_more.py
@@ -34,13 +34,13 @@ class Migration(migrations.Migration):
             name="startTime",
             field=models.CharField(default="", max_length=256),
         ),
-        migrations.AlterField(
-            model_name="event",
-            name="id",
-            field=models.BigAutoField(
-                auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
-            ),
-        ),
+        # migrations.AlterField(
+        #     model_name="event",
+        #     name="id",
+        #     field=models.BigAutoField(
+        #         auto_created=True, primary_key=True, serialize=False, verbose_name="ID"
+        #     ),
+        # ),
         migrations.AlterField(
             model_name="event",
             name="title",
